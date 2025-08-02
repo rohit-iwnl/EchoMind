d930f799669059c0bb77cdb2e4ff9ffcfc7ba937
rohit-iwnl <rohitmanivel9@gmail.com>
2025-08-02 14:00:06 -0700

    Initial Commit

---
+++ EchoMind/EchoMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoMindApp.swift
+//  EchoMind
+//
+//  Created by Rohit Manivel on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct EchoMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
